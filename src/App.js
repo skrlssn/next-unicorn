a2b2a1adc883982e3859fd36dc1c358d5562153a
Oskar Olsson <oskar.olsson@cygni.se>
2023-01-13 11:44:33 +0100

    Change to only fe

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and stuff
+          Learn React and stuff and more stuff
         </a>
       </header>
     </div>
